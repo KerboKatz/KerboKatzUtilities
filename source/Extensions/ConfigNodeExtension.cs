c7f953ab6017715651967f7f38d5a733b9f8ec6b
Xarun <Xarun@users.noreply.github.com>
2016-10-16 18:37:50 +0200

    1.4.5

--- source/Extensions/ConfigNodeExtension.cs
+++ source/Extensions/ConfigNodeExtension.cs
@@ -1,11 +1,23 @@
 ﻿namespace KerboKatz.Extensions
 {
-  public static partial class configNodeExtension
+  public static partial class ConfigNodeExtension
   {
-    public static float getFloatValue(this ConfigNode cfgNode, string name)
+    public static float GetFloatValue(this ConfigNode cfgNode, string name)
     {
       float value;
       float.TryParse(cfgNode.GetValue(name), out value);
+      return value;
+    }
+    public static int GetIntValue(this ConfigNode cfgNode, string name)
+    {
+      int value;
+      int.TryParse(cfgNode.GetValue(name), out value);
+      return value;
+    }
+    public static bool GetBoolValue(this ConfigNode cfgNode, string name)
+    {
+      bool value;
+      bool.TryParse(cfgNode.GetValue(name), out value);
       return value;
     }
   }
---
+++ source/Utilities/TimeUtils.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KerboKatz
+{
+  public class TimeUtils
+  {
+    public const int SecondsInMinute = 60;
+    public const int SecondsInHour = SecondsInMinute * 60;
+    public static int SecondsInDay
+    {
+      get
+      {
+        return SecondsInHour * HoursInDay;
+      }
+    }
+    public static int SecondsInYear
+    {
+      get
+      {
+        return SecondsInDay * DaysInYear;
+      }
+    }
+    public static int HoursInDay
+    {
+      get
+      {
+        return (GameSettings.KERBIN_TIME ? 6 : 24);
+      }
+    }
+    public static int DaysInYear
+    {
+      get
+      {
+        return (GameSettings.KERBIN_TIME ? 426 : 365);
+      }
+    }
+
+    public static int GetYears(double timeStamp)
+    {
+      return (int)Math.Floor(timeStamp / SecondsInYear);
+    }
+    public static int GetYears()
+    {
+      return GetYears(Planetarium.GetUniversalTime()) + 1;
+    }
+
+    public static int GetDays(double timeStamp)
+    {
+      return (int)Math.Floor(timeStamp % SecondsInYear / SecondsInDay);
+    }
+    public static int GetDays()
+    {
+      return GetDays(Planetarium.GetUniversalTime()) + 1;
+    }
+
+    public static int GetHours(double timeStamp)
+    {
+      return (int)Math.Floor(timeStamp % SecondsInDay / SecondsInHour);
+    }
+    public static int GetHours()
+    {
+      return GetHours(Planetarium.GetUniversalTime());
+    }
+
+    public static int GetMinutes(double timeStamp)
+    {
+      return (int)Math.Floor(timeStamp % SecondsInHour / SecondsInMinute);
+    }
+    public static int GetMinutes()
+    {
+      return GetMinutes(Planetarium.GetUniversalTime());
+    }
+
+    public static int GetSeconds(double timeStamp)
+    {
+      return (int)Math.Floor(timeStamp % SecondsInMinute);
+    }
+    public static int GetSeconds()
+    {
+      return GetSeconds(Planetarium.GetUniversalTime());
+    }
+  }
+}
